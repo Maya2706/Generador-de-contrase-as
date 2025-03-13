4e01f3e1705fc389a58e786692a9fd9405e7c28c
Maya Baytelman <maya.baytelman@maimonides.cl>
2025-03-12 21:08:09 -0300

    Add files via upload

---
+++ V2.py
@@ -0,0 +1,23 @@
+import random
+def generador():
+
+    caracteres = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    contrs = ""
+    x = int(input("Escribe la longitud de tu contraseña: "))
+
+
+
+    if x < 5:
+        while True:
+            x = int(input("Tu contrase;a es demasiado corta, escribe la longitud de tu contrase;a denuevo: "))
+            if x > 5:
+                break
+
+
+    for i in range(x):
+        caracter_aleatorio = random.choice(caracteres)
+        contrs += caracter_aleatorio
+
+
+    print("Contraseña generada", contrs)
+generador()
